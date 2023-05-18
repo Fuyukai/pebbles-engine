a80cfbf7371d0ec5b50411bd6379da621a04acc4
Lura Skye <l@veriny.tf>
2023-05-18 21:34:39 +0100

    add missing licence headers.

--- pebbles-engine/src/main/kotlin/tf/veriny/ss76/engine/psm/PsmFragmentManager.kt
+++ pebbles-engine/src/main/kotlin/tf/veriny/ss76/engine/psm/PsmFragmentManager.kt
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package tf.veriny.ss76.engine.psm
 
 import tf.veriny.ss76.engine.util.EktFiles
